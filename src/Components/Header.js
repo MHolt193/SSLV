f32b9badd623f41be103c78b08bf86f255137843
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-09-27 14:19:34 -0600

    Added NavBar and started setting page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import classes from './Header.module.css'
+import React from "react";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
-    return(
-        <div className={classes.container}>
-            <div className={classes['top-nav']}>
-                <p>SSLV</p>
-                <ul className={classes.list}>
-                    <li>Search</li>
-                    <li>Settings</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={classes.container}>
+      <div className={classes["top-nav"]}>
+        <p>SSLV</p>
+        <ul className={classes.list}>
+          <li>
+            <i className="fas fa-search"></i>
+          </li>
+          <li>
+            <i className="fas fa-cog"></i>
+          </li>
+        </ul>
+      </div>
+      <nav className={classes.nav}>
+        <ul className={classes["nav-list"]}>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Catagories</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;

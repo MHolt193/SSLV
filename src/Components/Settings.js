3bfb6deea3f1191b65fb1b0d1bc958fbba4ed91d
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-09-28 20:01:39 -0600

    refactored JSX and styled modal more

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import classes from './Settings.module.css'
+import React from "react";
+import classes from "./Settings.module.css";
 
-const Settings = (props) =>{
-    return(
-        <div className={classes.settings}> 
-            <div className={classes['settings-card']}>
-                <div className={classes['title-section']}>
-                <h2>Settings</h2>
-                </div>
-                <button onClick={props.settingsHandler}>Save</button>
-            </div> 
+const Settings = (props) => {
+  
+    const streamingServices = [
+    "Peacock",
+    "Netflix",
+    "HBO-Max",
+    "Hulu",
+    "Paramount+",
+    "Discovery+",
+    "Disney+",
+    "Prime-Video",
+  ];
+
+  return (
+    <div className={classes.settings}>
+      <div className={classes["settings-card"]}>
+        <div className={classes["title-section"]}>
+          <h2>Settings</h2>
+          <button
+            onClick={props.settingsHandler}
+            className={classes["close-button"]}
+          >
+            X
+          </button>
         </div>
-    )
+        <div className={classes.body}>
+          <p>Select your streaming services.</p>
+          <ul >
+            {streamingServices.map((service) => (
+              <li key={service} className={classes.services}>
+                <label htmlFor={service}>
+                  <input type="checkbox" id={service} />
+                  {service}
+                </label>
+              </li>
+            ))}
+          </ul>
+         
+        </div>
+        <div className={classes.footer}>
+         <button onClick={props.settingsHandler}>Save</button>
+        </div>
+      </div>
+      
+    </div>
+  );
 };
-
 
 export default Settings;

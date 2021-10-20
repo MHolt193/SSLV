5786e412afd00a296f4b83e8f66771ecc7505b2f
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-10-20 13:43:38 -0600

    removed play button an noon working style tag

--- src/Components/Home/Featured.js
+++ src/Components/Home/Featured.js
@@ -9,18 +9,13 @@ const Featured = (props) => {
   return (
     <div
       className={classes.featured}
-      stlye={{
-        backgroundImage: `url("http://img.omdbapi.com/?apikey=89a451f1&i=${props.results.titles[randomResult]["imdb_id"]}")`,
-      }}
     >
       <img
         src={`http://img.omdbapi.com/?apikey=89a451f1&i=${props.results.titles[randomResult]["imdb_id"]}`}
         alt={props.results.titles[randomResult].title}
       ></img>
-      <p>{props.results.titles[randomResult].title}</p>
       <div>
         <button>+ My List</button>
-        <button>Play</button>
         <button>Info</button>
       </div>
     </div>

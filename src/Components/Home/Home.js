ba4c82c80e12c1fb45657a9de388406c7e3cc084
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-10-21 14:07:50 -0600

    Added ability to save services and return results from only selected services

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,46 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback} from "react";
 import apiKey from "../../key";
 import classes from "./Home.module.css";
+import services from '../Settings/services'
 
 import MovieCard from "./MovieCard";
-import Featured from './Featured'
+import Featured from "./Featured";
 
 const Home = (props) => {
   const [apiResponse, setApiResults] = useState([]);
   const [apiRetrieved, setApiRetrieved] = useState(false);
 
-  useEffect(() => {
-    callApi();
-  }, []);
-  console.log(apiRetrieved);
-  const callApi = async () => {
+  
+
+  const callApi = useCallback(async () => {
+    const myServices = services.map((service) => localStorage.getItem(service.id) === 'true' ?
+  service.id + ',':'')
     const response = await fetch(
-      `https://api.watchmode.com/v1/list-titles/?apiKey=${apiKey}&source_ids=203,157,26,444`
+      `https://api.watchmode.com/v1/list-titles/?apiKey=${apiKey}&source_ids=${myServices}`
     );
     const data = await response.json();
     setApiResults(data);
     setApiRetrieved(true);
-  };
+  },[]);
+
+  useEffect(() => {
+    callApi();
+  }, [callApi]);
+  console.log(apiRetrieved);
+  console.log(apiResponse);
+
   return (
     <div className={classes.container}>
       <div>
-      {apiRetrieved === true && apiResponse.titles.length > 1
-        ? <Featured results={apiResponse}/>
-        : ""}
-        </div>
-        <div className={classes.list}>
-      {apiRetrieved === true && apiResponse.titles.length > 1
-        ? apiResponse.titles.map((title) => (
-            <MovieCard
-              imdbid={title["imdb_id"]}
-              imgalt={title.title}
-              key={title.id}
-            >
-              {title.title}
-            </MovieCard>
-          ))
-        : ""}
-        </div>
+        {apiRetrieved === true && apiResponse.titles.length > 1 ? (
+          <Featured results={apiResponse} />
+        ) : (
+          ""
+        )}
+      </div>
+      <div className={classes.list}>
+        {apiRetrieved === true && apiResponse.titles.length > 1
+          ? apiResponse.titles.map((title) => (
+              <MovieCard
+                imdbid={title["imdb_id"]}
+                imgalt={title.title}
+                key={title.id}
+              >
+                {title.title}
+              </MovieCard>
+            ))
+          : ""}
+      </div>
     </div>
   );
 };
--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,8 +1,8 @@
 import React from "react";
 import classes from "./Settings.module.css";
 import services from "./services";
-import { connect } from 'react-redux';
-import { setSelected, setUnselected } from '../redux/settings/settings.actions'
+import { connect } from "react-redux";
+import { setSelected, setUnselected } from "../redux/settings/settings.actions";
 
 const Settings = (props) => {
   const checkHandler = (event) => {
@@ -10,10 +10,10 @@ const Settings = (props) => {
       localStorage.setItem(event.target.id, true);
     }
     if (event.target.checked === false) {
-      localStorage.removeItem(event.target.id);
+      localStorage.setItem(event.target.id, false);
     }
   };
-  
+
   return (
     <div className={classes.settings}>
       <div className={classes["settings-card"]}>
@@ -29,35 +29,51 @@ const Settings = (props) => {
         <div className={classes.body}>
           <p>Select your streaming services.</p>
           <ul>
-            {services.map((service) => (
-              <li key={service.id} className={classes.services}>
-                <label htmlFor={service.id}>
-                  {service.name}
-                  <input
-                    type="checkbox"
-                    id={service.id}
-                    onClick={checkHandler}
-                  />
-                </label>
-              </li>
-            ))}
+            {services.map((service) =>
+              localStorage.getItem(service.id) === "true" ? (
+                <li key={service.id} className={classes.services}>
+                  <label htmlFor={service.id}>
+                    {service.name}
+                    <input
+                      type="checkbox"
+                      id={service.id}
+                      onClick={checkHandler}
+                      defaultChecked
+                    />
+                  </label>
+                </li>
+              ) : (
+                <li key={service.id} className={classes.services}>
+                  <label htmlFor={service.id}>
+                    {service.name}
+                    <input
+                      type="checkbox"
+                      id={service.id}
+                      onClick={checkHandler}
+                    />
+                  </label>
+                </li>
+              )
+            )}
           </ul>
         </div>
         <div className={classes.footer}>
-          <button onClick={props.settingsHandler}>Save</button>
+          <button type="submit" onClick={props.settingsHandler}>
+            Save
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-const mapStateToProps = state =>{
-  return{
+const mapStateToProps = (state) => {
+  return {
     selected: state.settings.selected,
-  }
-}
-const mapDispatchToProps = (dispatch) =>{
-  return{
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
     setSelected: () => dispatch(setSelected()),
     setUnselected: () => dispatch(setUnselected()),
   };

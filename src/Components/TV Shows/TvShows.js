079cc78c0748a2488587c44ced67772c3f916ffc
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-01-29 14:24:25 -0700

    titleinfo now working on tv shows section

--- src/Components/TV Shows/TvShows.js
+++ src/Components/TV Shows/TvShows.js
@@ -1,41 +1,60 @@
-import React, { useEffect, useState, useCallback} from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import apiKey from "../../key";
 import classes from "./TvShows.module.css";
-import services from '../Settings/services'
+import services from "../Settings/services";
+import TitleInfo from "../TitleInfo/TitleInfo"
 
 import MovieCard from "../Home/MovieCard";
 
 const TvShows = (props) => {
-  const [apiResponse, setApiResults] = useState([]);
-  const [apiRetrieved, setApiRetrieved] = useState(false);
-
-  
+  const [watchModeApiResponse, setWatchModeApiResults] = useState([]);
+  const [watchModeApiRetrieved, setWatchModeApiRetrieved] = useState(false);
 
   const callApi = useCallback(async () => {
-    const myServices = services.map((service) => localStorage.getItem(service.id) === 'true' ?
-  service.id + ',':'')
+    const myServices = services.map((service) =>
+      localStorage.getItem(service.id) === "true" ? service.id + "," : ""
+    );
     const response = await fetch(
       `https://api.watchmode.com/v1/list-titles/?apiKey=${apiKey}&source_ids=${myServices}&types=tv_series,tv_special,tv_miniseries`
     );
     const data = await response.json();
-    setApiResults(data);
-    setApiRetrieved(true);
-  },[]);
+    setWatchModeApiResults(data);
+    setWatchModeApiRetrieved(true);
+  }, []);
 
   useEffect(() => {
     callApi();
   }, [callApi]);
+  /* TITLE INFO */
+  const [titleInfoUp, controlTitleInfo] = useState(false);
+  const [titleId, getTitleId] = useState(undefined);
 
+  const titleInfoHandler = (event) => {
+    if (titleInfoUp === false) {
+      getTitleId(event.target.id);
+      controlTitleInfo(true);
+      console.log(event.target.id)
+    } else if (titleInfoUp === true) {
+      controlTitleInfo(false);
+    }
+  };
 
   return (
     <div className={classes.container}>
+      {titleInfoUp === true ? (
+          <TitleInfo titleInfoHandler={titleInfoHandler} id={titleId} />
+        ) : (
+          null
+        )}
       <div className={classes.list}>
-        {apiRetrieved === true && apiResponse.titles.length > 1
-          ? apiResponse.titles.map((title) => (
+        {watchModeApiRetrieved === true && watchModeApiResponse.titles.length > 1
+          ? watchModeApiResponse.titles.map((title) => (
               <MovieCard
+                onClick={titleInfoHandler}
                 imdbid={title["imdb_id"]}
                 imgalt={title.title}
                 key={title.id}
+                id={title.id}
               >
                 {title.title}
               </MovieCard>

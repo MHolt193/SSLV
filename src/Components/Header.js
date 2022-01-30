43b42a53537e78b296f94c3bfbb76ad10f302203
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-01-30 11:26:50 -0700

    added form with text input for start of searchbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,32 @@
 import React from "react";
 import classes from "./Header.module.css";
-import {Link} from 'react-router'
+import { Link } from "react-router";
 
 const Header = (props) => {
   return (
     <div className={classes.container}>
       <div className={classes["top-nav"]}>
-       <Link to='/'> <p>SSLV</p> </Link>
+        <Link to="/">
+          {" "}
+          <p>SSLV</p>{" "}
+        </Link>
         <ul className={classes.list}>
           <li>
-            <i className="fas fa-search"></i>
+            <form>
+              <input
+                type="text"
+                className={classes.search}
+                placeholder="NAME OF TITLE"
+                style={
+                  props.searchActive === true
+                    ? { display: "inline" }
+                    : { display: "none" }
+                }
+              />
+            </form>
+          </li>
+          <li>
+            <i className="fas fa-search" onClick={props.searchHandler}></i>
           </li>
           <li onClick={props.settingsHandler}>
             <i className="fas fa-cog"></i>
@@ -18,9 +35,16 @@ const Header = (props) => {
       </div>
       <nav className={classes.nav}>
         <ul className={classes["nav-list"]}>
-         <Link to='/Tv' ><li>TV Shows</li></Link>
-          <Link to='/movies'><li>Movies</li></Link>
-         <Link to='/Catagories'> <li>Catagories</li></Link>
+          <Link to="/Tv">
+            <li>TV Shows</li>
+          </Link>
+          <Link to="/movies">
+            <li>Movies</li>
+          </Link>
+          <Link to="/Catagories">
+            {" "}
+            <li>Catagories</li>
+          </Link>
         </ul>
       </nav>
     </div>

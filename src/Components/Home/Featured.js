6d7a65f4d7291a3d7ab1874293d8d650f4d87fc0
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-02-06 10:57:20 -0700

    reformatted with pretier

--- src/Components/Home/Featured.js
+++ src/Components/Home/Featured.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import classes from "./Featured.module.css";
 
 const Featured = (props) => {
@@ -11,17 +11,17 @@ const Featured = (props) => {
   const [apiRetrieved, setApiRetrieved] = useState(false);
   const callApi = useCallback(async () => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/find/${props.results.titles[randomMovie]['imdb_id']}?api_key=5f027ed6c33a834707d6d1b883944a4b&external_source=imdb_id`
+      `https://api.themoviedb.org/3/find/${props.results.titles[randomMovie]["imdb_id"]}?api_key=5f027ed6c33a834707d6d1b883944a4b&external_source=imdb_id`
     );
     const data = await response.json();
     setApiResults(data);
     setApiRetrieved(true);
-  }, [props.results.titles,randomMovie]);
+  }, [props.results.titles, randomMovie]);
   useEffect(() => {
     callApi();
   }, [callApi]);
 
-  let myList = props.myList
+  let myList = props.myList;
 
   const myListHandler = () => {
     myList.push({
@@ -35,17 +35,25 @@ const Featured = (props) => {
   return (
     <div className={classes.featured}>
       <h1 style={{ fontSize: 28 + "px" }}>Featured Title</h1>
-      {apiRetrieved === true && apiResponse['movie_results'].length  >= 1 ?
-      <img  id={props.id}
-        src={`http://image.tmdb.org/t/p/w300${apiResponse['movie_results'][0]['poster_path']}`}
-        alt={`${props.imgalt} poster`}
-      />: apiRetrieved === true && apiResponse['tv_results'].length >= 1 ?<img  id={props.id}
-      src={`http://image.tmdb.org/t/p/w300${apiResponse['tv_results'][0]['poster_path']}`}
-      alt={`${props.imgalt} poster`}
-    />:<img  id={props.id}
-    src={`http://img.omdbapi.com/?apikey=89a451f1&i=${props.results.titles[randomMovie]["imdb_id"]}`}
-    alt={`${props.results.titles[randomMovie].title}`}
-  />}
+      {apiRetrieved === true && apiResponse["movie_results"].length >= 1 ? (
+        <img
+          id={props.id}
+          src={`http://image.tmdb.org/t/p/w300${apiResponse["movie_results"][0]["poster_path"]}`}
+          alt={`${props.imgalt} poster`}
+        />
+      ) : apiRetrieved === true && apiResponse["tv_results"].length >= 1 ? (
+        <img
+          id={props.id}
+          src={`http://image.tmdb.org/t/p/w300${apiResponse["tv_results"][0]["poster_path"]}`}
+          alt={`${props.imgalt} poster`}
+        />
+      ) : (
+        <img
+          id={props.id}
+          src={`http://img.omdbapi.com/?apikey=89a451f1&i=${props.results.titles[randomMovie]["imdb_id"]}`}
+          alt={`${props.results.titles[randomMovie].title}`}
+        />
+      )}
       <div className={classes.buttons}>
         <button onClick={myListHandler}>+ My List</button>
         <button
